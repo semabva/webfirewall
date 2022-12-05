5b3a996493a2efb91e414a95adc95f76532efff5
semabva <114282202+semabva@users.noreply.github.com>
2022-12-05 14:26:49 +0400

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def qiymetleralma():
     return redirect(url_for('icaze',allow = icaze))
     
 if __name__ == "__main__":
-    app.run(debug=False,host = "0.0.0.0")
+    app.run(debug=False,host = "0.0.0.0",port 443)
 
 
 # pymysql, mysql-connector, mysql-connector-python
